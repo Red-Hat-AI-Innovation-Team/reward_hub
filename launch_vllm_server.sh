e55e378ffaf9c9b24374f404ab82d4448023980f
GX-Xu <gx.xu@ibm.com>
2024-05-13 12:42:39 -0400

    update ray distributed default

--- launch_vllm_server.sh
+++ launch_vllm_server.sh
@@ -5,6 +5,11 @@ if [ ! -d "logs" ]; then
     echo "Directory 'logs' created."
 fi
 
+ray disable-usage-stats
+export OPENBLAS_NUM_THREADS=18
+export OMP_NUM_THREADS=18
+ray start --head --num-cpus=32 --num-gpus=8
+
 
 # Accessing the first and second argument
 pref_model="$1"
@@ -13,19 +18,36 @@ ref_model="$2"
 echo "pref_model: $pref_model"
 echo "ref_model: $ref_model"
 
-CUDA_VISIBLE_DEVICES=5 python -u -m vllm.entrypoints.openai.api_server \
+CUDA_VISIBLE_DEVICES=0,1 python -u -m vllm.entrypoints.openai.api_server \
        --host 0.0.0.0 \
        --model $pref_model \
        --port 8010 \
-       --tensor-parallel-size 1 \
+       --tensor-parallel-size 2 \
        --load-format auto \
-       --download-dir ./download_dir > logs/server_0.log 2>&1 &
+       --download-dir /new_data/hf_cache > logs/server_0.log 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=7 python -u -m vllm.entrypoints.openai.api_server \
+CUDA_VISIBLE_DEVICES=2,3 python -u -m vllm.entrypoints.openai.api_server \
        --host 0.0.0.0 \
        --model $ref_model \
        --port 8011 \
-       --tensor-parallel-size 1 \
+       --tensor-parallel-size 2 \
        --load-format auto \
-       --download-dir ./download_dir > logs/server_1.log 2>&1 &
+       --download-dir /new_data/hf_cache > logs/server_1.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=4,5 python -u -m vllm.entrypoints.openai.api_server \
+       --host 0.0.0.0 \
+       --model $pref_model \
+       --port 8012 \
+       --tensor-parallel-size 2 \
+       --load-format auto \
+       --download-dir /new_data/hf_cache > logs/server_2.log 2>&1 &
+
+
+CUDA_VISIBLE_DEVICES=6,7 python -u -m vllm.entrypoints.openai.api_server \
+       --host 0.0.0.0 \
+       --model $ref_model \
+       --port 8013 \
+       --tensor-parallel-size 2 \
+       --load-format auto \
+       --download-dir /new_data/hf_cache > logs/server_3.log 2>&1 &
