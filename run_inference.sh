b1da0756e10d1765b258b6c33f9c0fe882c6f078
GX-Xu <gx.xu@ibm.com>
2024-05-25 19:03:20 -0400

    update save path names

--- run_inference.sh
+++ run_inference.sh
@@ -1,7 +1,10 @@
 
 # Define the sampling model
-model_engine="/dccstor/creme_brulee/aimodel_factory/instructlab/merlinite-deep-serenity-19_187264"
-model_engine="/new_data/gx/rejection_sampling_checkpoints/merlinite-deep-serenity-19/hf_format/samples_187264"
+# round1
+# model_engine="/new_data/gx/rejection_sampling_checkpoints/merlinite-deep-serenity-19/hf_format/samples_187264"
+# round2-sampling
+model_engine="/new_data/gx/iterative_rejection_sampling_checkpoints/round1/merlinite-summer-armadillo-24/hf_format/samples_157440"
+
 
 
 # Define the reward comparison models
@@ -10,8 +13,12 @@ ref_model="mistralai/Mixtral-8x7B-v0.1"
 
 # Define the input dataset path
 # input_data="/new_data/gx/synthetic_preference/uniform_sample_dataset_30k_best_of_64/bon_sampling_data_split_0.jsonl"
-input_data="/dccstor/gxamr/linux-386/llm-alignment/merlinite_RL_batches/uniform_sample_batch0.jsonl"
-input_data="/new_data/gx/synthetic_preference/merlinite_RL_batches/uniform_sample_batch0.jsonl"
+# input_data="/dccstor/gxamr/linux-386/llm-alignment/merlinite_RL_batches/uniform_sample_batch0.jsonl"
+
+# round-1, batch0
+input_data="/new_data/gx/synthetic_preference/merlinite_RL_batches/uniform_sample_batch1.jsonl"
+
+# round-2, batch1
 
 
 # Assign the first argument to SHARD_NUMS with a default of 1 if not provided
@@ -28,22 +35,16 @@ bash launch_sampling_server.sh $model_engine
 
 for bestn in 64; do
     filename_with_extension=$(basename "$input_data")
-
-    # Combining the directory path and the new subdirectory name
-    output_dir=$(dirname "$input_data")/$filename_with_extension
-
-    # Ensures the directory exists by creating it if it doesn't
-    mkdir -p "$output_dir"
-
-
-    # Remove the .jsonl extension
     filename="${filename_with_extension%.jsonl}"
+
+    output_dir=$(dirname "$input_data")/$filename-distribute
+    mkdir -p "$output_dir"  # Ensures the directory exists
 
     # Launch repeat_n_sampling with shard arguments
     python scripts/repeat_n_sampling.py \
     --decoder_name_or_path "$model_engine" \
     --base_port 8020 \
-    --output_path "$output_dir/best_of_${bestn}_shard_${SHARD_IDX}.jsonl" \
+    --output_path "$output_dir/best_of_${bestn}_distribute_shard_${SHARD_IDX}.jsonl" \
     --dataset_path "$input_data" \
     --num_return_sequences "$bestn" \
     --shard_nums "$SHARD_NUMS" \
