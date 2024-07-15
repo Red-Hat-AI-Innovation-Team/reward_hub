07135910241421e8cf336724b73480af9f88d804
GX Xu <gx.xu@ibm.com>
2024-07-15 19:17:32 +0000

    update script

--- run_annotate_test.sh
+++ run_annotate_test.sh
@@ -1,27 +1,14 @@
 # Define the reward comparison models
-pref_model="mistralai/Mixtral-8x7B-Instruct-v0.1"
-ref_model="mistralai/Mixtral-8x7B-v0.1"
+pref_model="RLHFlow/ArmoRM-Llama3-8B-v0.1"
+input_data="/new_data/gx/synthetic_preference/granite_RL_batches_new/uniform_sample_batch0-distribute/best_of_64_distribute_shard_0.jsonl"
 
-input_data="/new_data/gx/synthetic_preference/merlinite_RL_batches/uniform_sample_batch0/best_of_64_shard_0.jsonl"
-
-echo "Input data is: $input_data"
-
-bash launch_reward_server.sh
-
-bestn=64
 mkdir -p logs
 
-filename_with_extension=$(basename "$input_data")
-# Remove the .jsonl extension
-filename="${filename_with_extension%.jsonl}"
-
-# Run bon scoring
 python scripts/run_bon_scoring.py \
-    --model="$pref_model" \
-    --ref_model="$ref_model" \
-    --num_threads 1 \
-    --base_port 8020 \
-    --batch_size=8 \
-    --debug True \
-    --pref_sets $input_data
-
+--model="$pref_model" \
+--num_threads 8 \
+--model_type "classifier" \
+--batch_size=8 \
+--input_path $input_data \
+--debug True
+echo $!
