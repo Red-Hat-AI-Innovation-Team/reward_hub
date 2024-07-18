d25ba4b75d7f36a39614a97bd495ac99306b841d
GX Xu <gx.xu@ibm.com>
2024-07-18 14:57:00 +0000

    handling tokenizer differences

--- src/dpo_vllm.py
+++ src/dpo_vllm.py
@@ -2,6 +2,7 @@
 from .vllm_server import VLLM
 from multiprocessing import Process, Manager
 import torch 
+from transformers import AutoTokenizer
 import numpy as np
 
 
@@ -12,6 +13,7 @@ class DPOInferenceVLLM:
         self.max_prompt_length = max_prompt_length 
         self.engine = VLLM()
         self.tokenizer = tokenizer
+        self.ref_tokenizer = AutoTokenizer.from_pretrained(ref_model['model_name'])
 
     def inference_step(self, batch, average_log_prob=False):
         """_summary_
@@ -37,10 +39,13 @@ class DPOInferenceVLLM:
         chosen_batch, rejected_batch, prompt_batch = [ex["text_chosen"] for ex in batch], [ex["text_rejected"] for ex in batch], [ex["prompt"] for ex in batch]
 
         tokenized_prompt_batch = [self.tokenizer.encode(ex) for ex in prompt_batch]
-
+        ref_tokenize_prompt_batch = [self.ref_tokenizer.encode(ex) for ex in prompt_batch]
         # for each item in the tokenized batch; find the index of last non-pad token
         generation_first_token_indices = [
             len(ex) for ex in tokenized_prompt_batch
+        ]
+        ref_generation_first_token_indices = [
+            len(ex) for ex in ref_tokenize_prompt_batch
         ]
 
 
@@ -78,14 +83,18 @@ class DPOInferenceVLLM:
             chosen_logprob, chosen_ref_logprob, rejected_logprob, rejected_ref_logprob = \
                 np.array(chosen_logprobs[idx]), np.array(chosen_ref_logprobs[idx]), np.array(rejected_logprobs[idx]), np.array(rejected_ref_logprobs[idx])
             response_start_idx = generation_first_token_indices[idx]
+            ref_response_start_idx = ref_generation_first_token_indices[idx]
 
             # this thing produce a zero, in the case when response_start_idx is greater than the largest thing in the chosen_tokens
             chosen_unmask_indices = [
                 i for i, token in enumerate(chosen_tokens[idx]) if i >= response_start_idx and token != PAD_TOKEN
             ]
             rej_unmask_indices = [
-                i for i, token in enumerate(rejected_tokens[idx]) if i >= response_start_idx and token != PAD_TOKEN
+                i for i, token in enumerate(rejected_tokens[idx]) if i >= ref_response_start_idx and token != PAD_TOKEN
             ]
+            # this is a good sanity check;
+            # theoretically, they are only comparable if they have the same number of tokens. 
+            # assert len(chosen_unmask_indices) == len(rej_unmask_indices), "The number of tokens in the chosen and rejected responses are not the same"
 
             # sum(chosen_logprob[1:]) - sum(chosen_ref_logprob[1:])
             # sum(rejected_logprob[1:]) - sum(rejected_ref_logprob[1:])
@@ -95,7 +104,7 @@ class DPOInferenceVLLM:
             if average_log_prob:
                 chosen_rw = chosen_rw/len(chosen_unmask_indices)
                 rejected_rw = rejected_rw/len(rej_unmask_indices)
-            
+
             chosen_rewards.append(chosen_rw)
             rejected_rewards.append(rejected_rw)
 
@@ -149,7 +158,7 @@ class DPOInferenceVLLM:
             Process(target=fetch_logprobs, args=(chosen_batch, self.model["model_name"], self.model["port"], results, 'chosen_model')),
             Process(target=fetch_logprobs, args=(chosen_batch, self.ref_model["model_name"], self.ref_model["port"], results, 'chosen_ref_model')),
         ]
-        
+
         for process in processes:
             process.start()
 
@@ -160,13 +169,12 @@ class DPOInferenceVLLM:
 
         chosen_logprobs, chosen_ref_logprobs = results['chosen_model']["tokens_logprobs"], results['chosen_ref_model']["tokens_logprobs"]
         PAD_TOKEN = self.tokenizer.pad_token
-        
+
         chosen_rewards = []
         for idx in range(len(chosen_logprobs)):
             chosen_logprob, chosen_ref_logprob = \
                 np.array(chosen_logprobs[idx]), np.array(chosen_ref_logprobs[idx])
-            assert len(chosen_logprob) == len(chosen_ref_logprob), "Logprobs and ref logprobs should have the same length"
-            
+
             response_start_idx = generation_first_token_indices[idx]
             chosen_unmask_indices = [
                 i for i, token in enumerate(chosen_tokens[idx]) if i >= response_start_idx and token != PAD_TOKEN
