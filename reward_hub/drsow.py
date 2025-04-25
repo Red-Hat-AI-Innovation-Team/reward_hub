58a72dc4fb7050d9ec2e1a86d2d2a429bb2fa8b4
GX Xu <gxxu@redhat.com>
2025-04-25 17:13:44 +0000

    Remove redundant License text

--- reward_hub/drsow.py
+++ reward_hub/drsow.py
@@ -1,18 +1,3 @@
-# Copyright 2025 GX Xu (gxxu@redhat.com)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from .openai.vllm_client import vllmClient
 from multiprocessing import Process, Manager
 import torch 
@@ -130,8 +115,3 @@ class DrSow:
             })
 
         return final_reward_dicts
-
-
-if __name__ == "__main__":
-    # TODO: add test cases
-    pass
